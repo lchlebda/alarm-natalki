65944f4002dcb4d5cc572bbd49167f0442b186d6
aitseb <lchlebda@gmail.com>
2023-11-19 17:43:20 +0100

    Aktualizacja main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,19 @@
 input.onGesture(Gesture.ScreenUp, function () {
+    radio.sendString("alarm")
     for (let index = 0; index < 4; index++) {
         music.play(music.tonePlayable(988, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
         basic.pause(500)
     }
 })
 input.onGesture(Gesture.ScreenDown, function () {
+    radio.sendString("alarm")
     for (let index = 0; index < 4; index++) {
         music.play(music.tonePlayable(988, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
         basic.pause(500)
     }
 })
 input.onGesture(Gesture.ThreeG, function () {
+    radio.sendString("alarm")
     for (let index = 0; index < 4; index++) {
         music.play(music.tonePlayable(988, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
         basic.pause(500)
@@ -19,6 +22,7 @@ input.onGesture(Gesture.ThreeG, function () {
 radio.setGroup(255)
 basic.forever(function () {
     if (input.lightLevel() > 30) {
+        radio.sendString("alarm")
         basic.showIcon(IconNames.Angry)
         music.play(music.tonePlayable(988, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
     } else {
